73968341f6cfc79e1b88d0f3160a13f0cb49406b
Tomi <31417378+tomygespi@users.noreply.github.com>
2023-10-25 15:56:45 -0300

    Add swagger apiresponse and operation details

--- src/main/java/com/challenge/java/tomi/controller/TransactionController.java
+++ src/main/java/com/challenge/java/tomi/controller/TransactionController.java
@@ -8,6 +8,12 @@ import com.challenge.java.tomi.service.ITransactionService;
 import com.challenge.java.tomi.service.TransactionService;
 import java.util.List;
 import javax.validation.Valid;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,20 +27,69 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    @PutMapping("/transactions/{transactionId}")
+    @PutMapping(value = "/transactions/{transactionId}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new transaction from given ID and DTO")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Transaction created",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Transaction.class))}
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Bad request from transaction's wrong argument",
+            content = @Content
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Transaction not found",
+            content = @Content
+    )
+    @ApiResponse(
+            responseCode = "409",
+            description = "Transaction already exists for given transaction ID",
+            content = @Content
+    )
     public ResponseEntity<Transaction> create(
             @PathVariable Long transactionId,
             @Valid @RequestBody TransactionDTO transactionDTO) {
         return ResponseEntity.ok(this.transactionService.create(transactionId, transactionDTO));
     }
 
-    @GetMapping("/transactions/sum/{transactionId}")
+    @GetMapping(value = "/transactions/sum/{transactionId}",
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get nested transactions total amount by parent transaction ID")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Total amount sum",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = AmountSumDTO.class))}
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Transaction not found",
+            content = @Content
+    )
     public ResponseEntity<AmountSumDTO> getTotalAmountByParentTransaction(
             @PathVariable Long transactionId) {
         return ResponseEntity.ok(this.amountService.calculateTotalAmountByParentTransaction(transactionId));
     }
 
-    @GetMapping("/transactions/types/{type}")
+    @GetMapping(value = "/transactions/types/{type}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get all the transactions IDs by transaction type")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Total amount sum",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = List.class))}
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Type not supported",
+            content = @Content
+    )
     public ResponseEntity<List<Long>> getAllIdsByType(
             @PathVariable String type) {
         return ResponseEntity.ok(this.transactionService.findTransactionIdsByType(type));
